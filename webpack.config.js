79178fc3261dfc000db996c38ab5e633b503c948
djdifee <djdifee@gmail.com>
2018-02-28 10:34:49 +0100

    Changed publickPath according to survivejs gh-pages build guide

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
 	output: {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'build'),
-		publicPath: '/build/',
+		publicPath: '/home/',
 	},
 	resolve: {
 		extensions: ['.jsx', '.js']
