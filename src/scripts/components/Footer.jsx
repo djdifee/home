fdf4684934a3709936990bef2edb6243d97e121d
kristofferund <djdifee@gmail.com>
2018-07-02 14:24:16 +0200

    Added an example for sessionStorage

--- src/scripts/components/Footer.jsx
+++ src/scripts/components/Footer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { switchTheme } from '../service/actions/index';
@@ -11,43 +11,81 @@ import {
 const socialMedias = [
 	{
 		id: 'facebook',
-		link: 'https://www.facebook.com/djdifee/',
+		link: 'https://www.facebook.com/',
 		logo: IconFacebook,
 		name: 'Facebook',
 	},
 	{
 		id: 'instagram',
-		link: 'https://www.instagram.com/djdifee/',
+		link: 'https://www.instagram.com/',
 		logo: IconInstagram,
 		name: 'Instagram',
 	},
 	{
 		id: 'soundcloud',
-		link: 'https://soundcloud.com/dj-difee',
+		link: 'https://soundcloud.com/',
 		logo: IconSoundCloud,
 		name: 'SoundCloud',
 	},
 ];
 
+const storage = window.sessionStorage; // eslint-disable-line
+
 // When you don't need any functions inside of the component you should render a stateless component (basically just a function)
-const Footer = props => (
-	<div className="footer-content">
-		<div className="themes">
-			<span className="theme-button" onClick={() => props.switchTheme('winter')}>Winter</span>
-			<span className="theme-button" onClick={() => props.switchTheme('summer')}>Summer</span>
-		</div>
-		<div className="social-medias">
-			{/* You can map within the html to easier render lists */}
-			{socialMedias.map(social => (
-				<a key={social.id} target="_blank" rel="noopener noreferrer" href={social.link} className="social-icon">
-					{social.logo}
-				</a>
-			))}
-		</div>
-	</div>
-);
+class Footer extends Component {
+	constructor(props) {
+		super(props);
+		this.themeClicked = this.themeClicked.bind(this);
+		this.state = {};
+	}
+
+	componentDidMount() {
+		console.log('props: ', this.props);
+		console.log(storage);
+		if (storage.selectedTheme && !this.props.selectedTheme) {
+			storage.getItem('selectedTheme');
+			this.props.switchTheme(storage.selectedTheme);
+		} else {
+			console.warn('Local storage was not available');
+			this.props.switchTheme('winter');
+		}
+	}
+
+	render() {
+		return (
+			<div className="footer-content">
+				<div className="themes">
+					<span className="theme-button" onClick={() => this.themeClicked('winter')}>Winter</span>
+					<span className="theme-button" onClick={() => this.themeClicked('summer')}>Summer</span>
+				</div>
+				<div className="social-medias">
+					{/* You can map within the html to easier render lists */}
+					{socialMedias.map(social => (
+						<a key={social.id} target="_blank" rel="noopener noreferrer" href={social.link} className="social-icon">
+							{social.logo}
+						</a>
+					))}
+				</div>
+			</div>
+		);
+	}
+
+	themeClicked(selected) {
+		console.log('selected: ', selected);
+		console.log('storage: ', storage);
+		console.log('props: ', this.props);
+		if (storage) {
+			storage.setItem('selectedTheme', selected);
+			this.props.switchTheme(selected);
+		} else {
+			this.props.switchTheme(selected);
+		}
+	}
+}
+
 
 Footer.propTypes = {
+	selectedTheme: PropTypes.string,
 	switchTheme: PropTypes.func,
 };
 
@@ -55,4 +93,8 @@ const mapDispatchToProps = dispatch => ({
 	switchTheme: payload => dispatch(switchTheme(payload)),
 });
 
-export default connect(null, mapDispatchToProps, null)(Footer);
+const mapStateToProps = state => ({
+	selectedTheme: state.switchTheme.selectedTheme,
+});
+
+export default connect(mapStateToProps, mapDispatchToProps, null)(Footer);
